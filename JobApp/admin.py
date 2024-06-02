b9ff6100d680dfe0448507485079ce9d7eb057c4
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-03 03:39:15 +0600

    [ADDED] forms connected to database

--- JobApp/admin.py
+++ JobApp/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Job
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'date', 'ocupation')
+    list_display = ('first_name', 'last_name', 'email', 'date', 'occupation')
 
 admin.site.register(Job, JobAdmin)
--- JobApp/views.py
+++ JobApp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .forms import JobForm
+from .models import Job
 
 
 def index(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            
+            job = Job.objects.create(
+                first_name=first_name, 
+                last_name=last_name, 
+                email=email, 
+                date=date, 
+                occupation=occupation
+            )
+            job.save()
+
+        
     return render(request, 'index.html')
