4ed5330f749a15edd99b362ad2bb6569ee66d54b
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-03 03:55:13 +0600

    [ADDED] on refresh resubmit fixed

--- JobApp/views.py
+++ JobApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import JobForm
 from .models import Job
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -13,14 +15,26 @@ def index(request):
             date = form.cleaned_data['date']
             occupation = form.cleaned_data['occupation']
             
-            job = Job.objects.create(
+            Job.objects.create(
                 first_name=first_name, 
                 last_name=last_name, 
                 email=email, 
                 date=date, 
                 occupation=occupation
             )
-            job.save()
+
+            msg_body = f"Thank you for expressing interest in the position, {first_name}. We appreciate your application and will review it carefully. Expect to hear from us soon regarding the next steps in the hiring process. Here is your data: \n\nFirst Name: {first_name}\nLast Name: {last_name}\nEmail: {email}\nOccupation: {occupation}\nDate: {date}. \nThank you!"
+
+            # email = EmailMessage(
+            #     'Job Application',
+            #     reply_to=[email], 
+            #     body=msg_body
+            # )
+
+            # email.send()
+
+            messages.success(request, 'Your application has been submitted successfully!')
+
             return redirect(reverse('index'))
         
     return render(request, 'index.html')
